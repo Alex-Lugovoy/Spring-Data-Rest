460b1b970326ff9910e4356ba3f68a6b6bd2ccfb
alex <alex@alex-Z370P-D3>
2022-04-01 20:46:22 +0300

    finish

---
+++ spring_data_rest/src/main/java/com/alex/spring/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.alex.spring.spring_data_rest.dao;
+
+
+
+import com.alex.spring.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByName(String name);
+}
